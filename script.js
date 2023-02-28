f3f85f9ff70f6761d246d1539257f38beda1463d
lamonkey <88therisingsun@gmail.com>
2023-02-28 02:44:47 -0500

    added floating modal

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-//setting image and canvas size 
+//setting image and canvas size
 const maxImgheight = 300;
 const padding_between_images = 10;
-
-
-
 
 //create a hidden floating window
 let floatingWindow = document.createElement("div");
@@ -33,7 +30,6 @@ downloadButton.setAttribute("type", "button");
 // Set the text of the button to 'Download'
 downloadButton.innerText = "Download";
 
-
 //add all to floating window
 floatingWindow.appendChild(selectedCounter);
 floatingWindow.appendChild(InputLabel);
@@ -41,7 +37,50 @@ floatingWindow.appendChild(numberInput);
 floatingWindow.appendChild(downloadButton);
 document.body.appendChild(floatingWindow);
 
-var canvas = null
+//create a modal box
+const modalBox = document.createElement("div");
+modalBox.style.display = "none";
+modalBox.style.position = "fixed";
+modalBox.style.zIndex = 1;
+modalBox.style.paddingTop = "100px";
+modalBox.style.left = 0;
+modalBox.style.top = 0;
+modalBox.style.width = "100%";
+modalBox.style.height = "100%";
+modalBox.style.overflow = "auto";
+modalBox.style.backgroundColor = "rgb(0,0,0)";
+modalBox.style.backgroundColor = "rgba(0,0,0,0.4)";
+
+//modal content
+const modalContent = document.createElement("div");
+modalContent.style.backgroundColor = "#fefefe";
+modalContent.style.margin = "auto";
+modalContent.style.padding = "20px";
+modalContent.style.border = "1px solid #888";
+modalContent.style.width = "80%";
+
+// modal close
+const closeBtn = document.createElement("span");
+closeBtn.innerHTML = "&times;";
+closeBtn.style.color = "#aaaaaa";
+closeBtn.style.float = "right";
+closeBtn.style.fontSize = "28px";
+closeBtn.style.fontWeight = "bold";
+
+//modal content canvas
+const modalCanvas = document.createElement("canvas");
+modalCanvas.id = "modalCanvas";
+//add everything to modal
+modalContent.appendChild(modalCanvas);
+modalContent.appendChild(closeBtn);
+modalBox.appendChild(modalContent);
+//add modal box to body
+document.body.appendChild(modalBox);
+
+function close_modal() {
+  modalBox.style.display = "none";
+}
+
 function toggle_images(event) {
   console.log(event.target.src);
   if (event.target.style.filter === "grayscale(80%) opacity(0.7)") {
@@ -50,12 +89,12 @@ function toggle_images(event) {
     if (index > -1) {
       highlightedImages.splice(index, 1);
     }
-    event.target.style.filter = '';
+    event.target.style.filter = "";
     event.target.style.zIndex = 2;
   } else {
     // Add the image to the list
     highlightedImages.push(event.target);
-    event.target.style.filter = 'grayscale(80%) opacity(0.7)';
+    event.target.style.filter = "grayscale(80%) opacity(0.7)";
     event.target.style.zIndex = 2;
   }
 }
@@ -83,17 +122,17 @@ function generate_floating_windows(e) {
 document.addEventListener("click", function (event) {
   if (event.target.id === "floatingwindow") {
     //do nothing
-  }
-  else if (event.target.tagName === "IMG") {
+  } else if (event.target.tagName === "IMG") {
     toggle_images(event);
     generate_floating_windows(event);
+  } else if (event.target == closeBtn) {
+    close_modal();
   }
-  
 });
 
 //download selected images in a grid
-function generate_photo_grid(imgPerRow, canvas) {
-  var ctx = canvas.getContext("2d");
+function generate_photo_grid(imgPerRow) {
+  var ctx = modalCanvas.getContext("2d");
   const numOfImgs = highlightedImages.length;
   var x = 0;
   var y = 0;
@@ -102,80 +141,80 @@ function generate_photo_grid(imgPerRow, canvas) {
     const scale = maxImgheight / highlightedImages[i].height;
     const width = highlightedImages[i].width * scale;
     ctx.drawImage(highlightedImages[i], x, y, width, maxImgheight);
-    x += width+padding_between_images;
-    x = Math.floor(x)
-    if ((i+1) % imgPerRow == 0) {
+    x += width + padding_between_images;
+    x = Math.floor(x);
+    if ((i + 1) % imgPerRow == 0) {
       x = 0;
       y += maxImgheight + padding_between_images;
-      y = Math.floor(y)
+      y = Math.floor(y);
     }
   }
   console.log("canvas created");
 }
 function get_canvas_height_and_width(numPicPerRow) {
-    let height = 0;
-    let width = 0;
-    let current_row_width = 0;
-    let rowCount = 1;
-    //loop thought each image
-    for (var i = 0; i < highlightedImages.length; i++) {
-        //when current image is add to a new row
-        if ((i+1) % numPicPerRow == 0) {
-            height += maxImgheight;
-            current_row_width = 0;
-            rowCount++;
-        }
-        //get scale make height == maxImgheight
-        const scale = maxImgheight / highlightedImages[i].height;
-        //scale width 
-        const scaledWidth = highlightedImages[i].width * scale;
-        current_row_width += scaledWidth;
-        width = width < current_row_width ? current_row_width : width;
-        }
-    return {height: height, width: width, rowCount: rowCount};
+  let height = 0;
+  let width = 0;
+  let current_row_width = 0;
+  let rowCount = 1;
+  //loop thought each image
+  for (var i = 0; i < highlightedImages.length; i++) {
+    //when current image is add to a new row
+    if ((i + 1) % numPicPerRow == 0) {
+      height += maxImgheight;
+      current_row_width = 0;
+      rowCount++;
+    }
+    //get scale make height == maxImgheight
+    const scale = maxImgheight / highlightedImages[i].height;
+    //scale width
+    const scaledWidth = highlightedImages[i].width * scale;
+    current_row_width += scaledWidth;
+    width = width < current_row_width ? current_row_width : width;
+  }
+  return { height: height, width: width, rowCount: rowCount };
 }
 
 function create_an_canvas(setting) {
-  let canvas = document.createElement("canvas");
-  canvas.width = setting.width + (setting.rowCount-1) * padding_between_images;
-  canvas.height = setting.height + (setting.rowCount-1) * padding_between_images;
-  canvas.style.display = "none";
-  canvas.style.zIndex = "100000"; // just a large enough number
-  canvas.style.position = "fixed";
-  canvas.style.border = "1px solid black";
-  canvas.id = "myCanvas";
-  const parentElement = document.getElementsByTagName("body")[0];
-  parentElement.appendChild(canvas);
-  return canvas;
+    modalCanvas.width =
+    setting.width + (setting.rowCount - 1) * padding_between_images;
+    modalCanvas.height =
+    setting.height + (setting.rowCount - 1) * padding_between_images;
+    // modalCanvas.style.display = "none";
+    // modalCanvas.style.zIndex = "100000"; // just a large enough number
+    // modalCanvas.style.position = "fixed";
+    modalCanvas.style.border = "1px solid black";
+    // modalCanvas.id = "myCanvas";
+//   const parentElement = document.getElementsByTagName("body")[0];
+//   parentElement.appendChild(canvas);
+//   return canvas;
 }
-
 
 // download created canvas
 function download_canvas(e) {
   //if value not define then set to 1
-  const numPicPerRow = document.getElementById("numPicPerRow").value ? document.getElementById("numPicPerRow").value : 1;
-// const numPicPerRow = document.getElementById("numPicPerRow").value;
-  // updateFloatingWindowPos = false;
-  console.log("numPicPerRow: " + numPicPerRow);
+  const numPicPerRow = document.getElementById("numPicPerRow").value
+    ? document.getElementById("numPicPerRow").value
+    : 1;
 
   //get canvas height and width
   const canvasSetting = get_canvas_height_and_width(numPicPerRow);
-  canvas = create_an_canvas(canvasSetting);
-  generate_photo_grid(numPicPerRow, canvas);
+  create_an_canvas(canvasSetting);
+  generate_photo_grid(numPicPerRow);
   try {
-    canvas.toBlob(function (blob) {
+    modalCanvas.toBlob(function (blob) {
       const link = document.createElement("a");
       link.href = URL.createObjectURL(blob);
       link.download = "my-image.png";
       link.click();
     });
+  } catch (e) {
+    modalBox.style.display = "block";
+    modalCanvas.style.left = e.clientX + "px";
+    modalCanvas.style.top = e.clientY + "px";
+    alert(
+      "cannot download canvas due to image from external source, generated on the webpage instead"
+    );
   }
-    catch (e) {
-        canvas.style.display="block";
-        canvas.style.left = e.clientX + "px";
-        canvas.style.top = e.clientY + "px";
-        alert( "cannot download canvas due to image from external source, generated on the webpage instead" );
-    }
-//    highlightedImages = [];
+  //    highlightedImages = [];
 }
 downloadButton.addEventListener("click", download_canvas);
